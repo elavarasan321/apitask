12d8433ed66e344abba7c5d9e4ba706fcb96b999
elavarasan321 <elavarasan@bititude.com>
2021-04-26 12:05:42 +0530

    dist ignore

--- server/app.js
+++ server/app.js
@@ -2,11 +2,10 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import indexRouter from "./routes/items/index";
+import Routes from "./routes";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import usersRouter from "./routes/items/index";
 import { dirname } from "path";
 dotenv.config();
 
@@ -23,9 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+Routes(app);
 
 export default app;
+
+
+
